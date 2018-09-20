5fca3a6a69b7c3494c43f845e74f041161449913
Rakesh Gujari <rakeshgujari123@gmail.com>
2018-09-21 00:17:58 +0530

    Initial Commit

---
+++ SmaaashSDKExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmaaashSDKExample
+//
+//  Created by Rakesh Gujari on 21/09/18.
+//  Copyright © 2018 Rakesh Gujari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
