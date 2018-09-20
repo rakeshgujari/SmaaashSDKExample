98cb78e2b7d6aa0be5dffade4add8d97cd98849c
Rakesh Gujari <rakeshgujari@Rakeshs-MacBook-Air.local>
2018-09-21 01:02:47 +0530

    First Commit

---
+++ SmaaashSDK.framework/Headers/SmaaashSDK.h
@@ -0,0 +1,19 @@
+//
+//  SmaaashSDK.h
+//  SmaaashSDK
+//
+//  Created by Rakesh Gujari on 20/09/18.
+//  Copyright © 2018 Rakesh Gujari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SmaaashSDK.
+FOUNDATION_EXPORT double SmaaashSDKVersionNumber;
+
+//! Project version string for SmaaashSDK.
+FOUNDATION_EXPORT const unsigned char SmaaashSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SmaaashSDK/PublicHeader.h>
+#import <SmaaashSDK/ACRCloudConfig.h>
+#import <SmaaashSDK/ACRCloudRecognition.h>
--- SmaaashSDKExample/ViewController.swift
+++ SmaaashSDKExample/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import SmaaashSDK
 
 class ViewController: UIViewController {
 
+    var resultView: UITextView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        resultView = UITextView(frame: CGRect(x: 50, y: 10, width: 200, height: 400))
+        self.view.addSubview(resultView!)
+        
+        
+        SmaaashSDK.default.setConfig(_apiKey: "")
+        SmaaashSDK.default.start { (result) in
+            DispatchQueue.main.async {
+                self.resultView?.text = result
+            }
+        }
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
